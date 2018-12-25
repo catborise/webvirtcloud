a664352d4d20e38241f1fd5f8d1e93cd3baa6ad0
catborise <catborise@yahoo.com>
2018-12-25 15:37:11 +0300

    External snapshot delete/revert/delete all

--- instances/models.py
+++ instances/models.py
@@ -11,3 +11,29 @@ class Instance(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Snapshot(models.Model):
+    id = models.AutoField(primary_key=True)
+    instance = models.ForeignKey(Instance, on_delete=models.CASCADE)
+    display_name = models.CharField("snapshot name", unique=True, max_length=25)
+    num_disk = models.IntegerField("number of disks")
+    date = models.DateTimeField("creation Time")
+    description = models.CharField("snapshot description", blank=True, null=True, max_length=200)
+    deleted = models.BooleanField("has it deletable disks", default=False)
+    current = models.BooleanField("is it current snapshot")
+
+    def __unicode__(self):
+        return self.display_name
+
+
+class SnapshotDisk(models.Model):
+    snapshot = models.ForeignKey(Snapshot, on_delete=models.CASCADE)
+    snap_type = models.CharField("snapshot type", blank=True, max_length=15)
+    source = models.FilePathField("disk filename with path")
+    dev = models.CharField("disk dev name", max_length=5)
+    driver = models.CharField("disk driver type", blank=True, max_length=5)
+    parent = models.CharField("disk backing file", blank=True, max_length=200)
+    type = models.CharField("disk type", blank=True, max_length=5)
+
+
